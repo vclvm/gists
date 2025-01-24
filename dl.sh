3532453d005dde82fdc1b1a0f7f2cbf7626d4e67
vclvm <vclvm@proton.me>
2025-01-24 12:25:29 +0100

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-folder="$HOME/apps"
+file_name="Hiddify-Debian-x64.deb"
+url="https://objects.githubusercontent.com/github-production-release-asset-2e65be/643504282/592ec478-72f7-4fee-bba4-fd78ecadbd35?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250124%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250124T112232Z&X-Amz-Expires=300&X-Amz-Signature=a2fdd30c34e8e00aa84d9fd193ca884f3ff842381f1d834fa18ebada69308ce1&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DHiddify-Debian-x64.deb&response-content-type=application%2Foctet-stream"
 
-if [ ! -d "$folder" ]; then
-  mkdir -p "$folder"
-fi
-
-url="https://objects.githubusercontent.com/github-production-release-asset-2e65be/643504282/44ca98d7-fe62-4ede-b21b-25544b941b6e?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250124%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250124T104844Z&X-Amz-Expires=300&X-Amz-Signature=83288c8ca2cd922ead8b0e7d69f1483e7d29ec8e20846386c4f1cd632e893f84&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DHiddify-Linux-x64.AppImage&response-content-type=application%2Foctet-stream"
-
-wget -P "$folder" "$url"
-
-echo "Download complete."
+wget -O "$file_name" "$url" && sudo dpkg -i "$file_name"
